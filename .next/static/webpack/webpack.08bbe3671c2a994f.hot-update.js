2924a22cd4f935aefac962e8292996892eadd3bc
Deepak jain <deeepakjain132@gmail.com>
2024-02-24 22:22:39 +0530

    fix

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9afd7ac6663f7f61"; }
+/******/ 	__webpack_require__.h = function() { return "38314f43f5e39fd8"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
